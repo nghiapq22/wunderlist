7056a51cd3d777281687e9582cc3ef9b4389612b
nghiapq22 <nghiapq62@wru.vn>
2020-07-09 14:09:10 +0700

    delete

--- deleteComment.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require('ketnoi.php');
-    $cmtId = $_POST['cmtId'];
-    $sql = "DELETE from comment where id = $cmtId";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-?>
-?>
--- deleteSubTask.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require('ketnoi.php');
-    $subTaskId = $_POST['subTaskId'];
-    $sql = "DELETE from subtask where id = '$subTaskId'";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-?>
